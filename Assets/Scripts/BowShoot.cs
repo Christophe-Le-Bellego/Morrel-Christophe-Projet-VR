8da718f2300d17444616d4f0b6230026f01234b1
Christophe-Le-Bellego <christophe.lebellego@gmail.com>
2025-12-17 20:35:07 +0100

    commit pour modifier le .gitignore

--- Assets/Scripts/BowShoot.cs
+++ Assets/Scripts/BowShoot.cs
@@ -5,13 +5,44 @@ public class BowShoot : MonoBehaviour
     [SerializeField] GameObject arrowPrefab;
     [SerializeField] Transform firePoint;
     [SerializeField] float arrowSpeed = 50f;
+    [SerializeField] float maxDistance = 100f; // utilise pour le raycast
+    [SerializeField] LineRenderer laserLine;
+
+
+void Update()
+    {
+        AfficherLaser();
+    }
+
+    void AfficherLaser()
+    {
+        // 1. Point de départ : Le bout de l'arc
+        laserLine.SetPosition(0, firePoint.position);
+
+        // 2. Calcul de la trajectoire
+        Ray ray = new Ray(firePoint.position, firePoint.right);
+        RaycastHit hit;
+
+        // 3. Point d'arrivée
+        if (Physics.Raycast(ray, out hit, maxDistance))
+        {
+            // Si on touche un mur/ennemi, le laser s'arrête dessus
+            laserLine.SetPosition(1, hit.point);
+        }
+        else
+        {
+            // Si on ne touche rien, le laser va loin devant (maxDistance)
+            laserLine.SetPosition(1, firePoint.position + (firePoint.right * maxDistance));
+        }
+    }
+
 
 
 
 
     public void Shoot()
     {
-        
+  
         //arrowPrefab.transform.Rotate(90, 0, 0);
         //firePoint.Rotate(0, 90, 0);
         GameObject arrow = Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
